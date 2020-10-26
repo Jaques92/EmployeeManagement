da8ede58d5d4910b2485de4ce7d2cb22439fc0fa
Tiaan Stassen <tiaan@chasesoftware.co.za>
2020-10-26 16:04:30 +0200

    role edit fix

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -87,7 +87,7 @@ namespace EmployeeManagement.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("EmployeeID,EmployeeName,EmployeeSurnameName,EmployeeEmail,EmployeePassword,EmployeeStartDate,EmployeeEndDate,EmployeeImage,RoleID")] Employee employee)
+        public async Task<IActionResult> Edit(int id, [Bind("EmployeeID,EmployeeName,EmployeeSurname,EmployeeEmail,EmployeePassword,EmployeeStartDate,EmployeeEndDate,EmployeeImage,RoleID")] Employee employee)
         {
             if (id != employee.EmployeeID)
             {
