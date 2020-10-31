8bfd067aa682198f849f6b99cfa98eee0669fbcc
Jaques92 <jaques92greyling@gmail.com>
2020-10-31 09:35:45 +0200

    Start Salary calc

---
+++ Models/SalaryModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class SalaryModel
+    {
+        public int EmployeeID { get; set; }
+
+        public DateTime SalaryStartDate { get; set; }
+
+        public DateTime SalaryEndDate { get; set; }
+    }
+}
